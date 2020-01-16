9649760b0f845922f9bbe73b5b1f5fecd7cddf5b
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-15 17:37:22 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,20 @@
-print('hello')
-print('world')
-print('')
+count = 0
+def example3():
+    count = count + 1   
+    
+    
+def exmple3():
+    global count 
+    count += 1
+    
+ known = {0:0, 1:1}
+ 
+ def example 4():
+     known[2] = 1
+    
+ def example5():
+     global known
+     known = dict()
+    
+    
+
